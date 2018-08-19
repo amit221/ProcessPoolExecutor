dd23bdc5df1e46331b0524e7851d4336de11c7a4
amitvagner@gmail.com <amitvagner@gmail.com>
2018-08-19 15:37:07 +0300

    refcator code to work better with promises and handle more child process functions

--- src/ProcessPoolExecutor.ts
+++ src/ProcessPoolExecutor.ts
@@ -1,5 +1,18 @@
-import { cpus } from "os";
-import { exec } from "child_process";
+import {cpus} from "os";
+import {exec} from "child_process";
+import {Error} from "tslint/lib/error";
+
+const execPromise = cmd => {
+    return new Promise((resolve, reject) => {
+        exec(cmd, (err, stdout, stderr) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve({stdout, stderr});
+        });
+    });
+};
+
 
 type IResolveFn = (buffer: IBufferOutput) => void;
 type IRejectFn = (err: Error) => void;
@@ -10,61 +23,57 @@ interface IBufferOutput {
 }
 
 interface IDeferedCmd {
-  cmd: string,
-  name: string,
-  resolveFn: any,
-  rejectFn: any,
+    type: string,
+    cmd: string,
+    name: string,
+    resolve: IResolveFn,
+    reject: IRejectFn
+
 }
 
 export class ProcessPoolExecutor {
-  private static cpuCounts = cpus().length;
-  private static deferedCmds: IDeferedCmd[] = [];
-  private static usedCpus = 0;
+    private static cpuCounts = cpus().length;
+    private static deferedCmds: IDeferedCmd[] = [];
+    private static usedCpus = 0;
 
-  static execute = async (cmd: string, name: string, resolveCb?: IResolveFn, rejectCb?: IRejectFn) =>
-    new Promise((resolveArg, rejectArg) => {
-      let resolve: IResolveFn;
-      let reject: IRejectFn;
+    private static nextCmd = () => {
+        if (ProcessPoolExecutor.deferedCmds.length > 0) {
+            const firstDeferedCmd = ProcessPoolExecutor.deferedCmds.pop();
+            ProcessPoolExecutor[firstDeferedCmd.type](firstDeferedCmd);
+        }
+    };
 
-      if (resolveCb && rejectCb) {
-        resolve = resolveCb;
-        reject = rejectCb;
-      } else {
-        resolve = resolveArg;
-        reject = rejectArg;
-      }
+    static execute = async ({cmd, name}: { cmd: string, name: string }) => {
 
-      if (ProcessPoolExecutor.usedCpus < ProcessPoolExecutor.cpuCounts) {
-        ProcessPoolExecutor.usedCpus++;
-        exec(cmd, async (err, stdout, stderr) => {
-          if (err) {
-            reject(err);
-          }
+        if (ProcessPoolExecutor.usedCpus < ProcessPoolExecutor.cpuCounts) {
+            ProcessPoolExecutor.usedCpus++;
+            try {
+                const results = await exec(cmd);
+                ProcessPoolExecutor.usedCpus--;
+                ProcessPoolExecutor.nextCmd();
+                return results;
+            }
+            catch (err) {
+                ProcessPoolExecutor.usedCpus--;
+                ProcessPoolExecutor.nextCmd();
+                throw err;
+            }
+        }
 
-          ProcessPoolExecutor.usedCpus--;
-          resolve({
-            stdout,
-            stderr,
-          });
-          
-          if(ProcessPoolExecutor.deferedCmds.length > 0) {
-            const [firstDeferedCmd, ...rest] = ProcessPoolExecutor.deferedCmds;
-            ProcessPoolExecutor.deferedCmds = rest.slice();
-            ProcessPoolExecutor.execute(
-              firstDeferedCmd.cmd,
-              firstDeferedCmd.name,
-              firstDeferedCmd.resolveFn,
-              firstDeferedCmd.rejectFn,
-            );
-          }
+        const deferedCmdPromise = new Promise((resolve, reject) => {
+            ProcessPoolExecutor.deferedCmds.push({
+                type: "execute",
+                cmd,
+                name,
+                resolve,
+                reject
+            });
         });
-      } else {
-        ProcessPoolExecutor.deferedCmds.push({
-          cmd,
-          name,
-          resolveFn: resolve,
-          rejectFn: reject,
-        });
-      }
-    });
+
+        return deferedCmdPromise;
+
+
+    };
+
+
 }
